488df204d44dd3d9c2a1584ffc84f73c4aee74c3
anukollon <anukollon@gmail.com>
2020-11-12 15:06:36 -0600

    Added a 'Add User' feature

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            Console.WriteLine("username-"+user.Username+"-");
+            ViewBag.name = user.Username;
+            ViewBag.email = user.Email;
+
+            if (verify == user.Password)
+            {
+                ViewBag.time = user.TimeJoined;
+                return View("index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords does not match. Try again!";
+                Redirect("/User/Add");
+            }
+            return View();
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime TimeJoined { get; set; }
+
+        public User()
+        {
+            TimeJoined = DateTime.Now;
+        }
+
+        public User(string username, string email, string password):this()
+        {
+            Console.WriteLine("Inside constructor");
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
